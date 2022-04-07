6d6ce77830f1dc9915445bfe1bb6e8947660d0d3
smartdevb16 <smartdevb16@gmail.com>
2022-04-07 11:27:58 +0300

    velamis token contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,113 @@
 require("@nomiclabs/hardhat-waffle");
+require('hardhat-deploy');
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-etherscan");
+const fs = require('fs');
+const projectId = "e27fa5472f26967c3f445b1e";
+const privateKey ="caf9ee74b3d7a043aa9ccfa04563f2de9c6d621703c2c878f72fbba5c28d4a6e";
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  // defaultNetwork: "rinkeby",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      // chainId: 4 //ethereum
+      chainId: 1337, //ethereum
+      // chainId: 97, //ethereum
+    },
+    polygonmainnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${projectId}/polygon/mainnet`,
+      accounts: [privateKey]
+    },
+    mumbai: {
+      url: `https://speedy-nodes-nyc.moralis.io/${projectId}/polygon/mumbai`,
+      accounts: [privateKey]
+    },
+    ethermainnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${projectId}/eth/mainnet`,
+      accounts: [privateKey]
+    },
+    kovan: {
+      url: `https://speedy-nodes-nyc.moralis.io/${projectId}/eth/kovan`,
+      accounts: [privateKey]
+    },
+    rinkeby: {
+      url: `https://speedy-nodes-nyc.moralis.io/${projectId}/eth/rinkeby`,
+      accounts: [privateKey]
+    },
+    goerli: {
+      url: `https://speedy-nodes-nyc.moralis.io/${projectId}/eth/goerli`,
+      accounts: [privateKey]
+    },
+    bscmainnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${projectId}/bsc/mainnet`,
+      accounts: [privateKey]
+    },
+    bsctestnet: {
+      url: `https://speedy-nodes-nyc.moralis.io/${projectId}/bsc/testnet`,
+      accounts: [privateKey],
+      gasPrice: 50000000000
+    }
+  },
+  paths:{
+    sources: "./contracts",
+    artifacts: "./artifacts"
+  },
+ 
+  solidity: {
+    compilers: [
+      {
+      	version: "0.8.10",
+      	settings: {
+      		optimizer: {
+      			enabled: true,
+      			runs: 200,
+      		},
+      	}
+      },
+      // {
+      // 	version: "0.4.17",
+      // 	settings: {
+      // 		optimizer: {
+      // 			enabled: true,
+      // 			runs: 200,
+      // 		},
+      // 	}
+      // },
+      // {
+      // 	version: "0.5.16",
+      // 	settings: {
+      // 		optimizer: {
+      // 			enabled: true,
+      // 			runs: 200,
+      // 		},
+      // 	}
+      // },
+      {
+      	version: "0.8.4",
+      	settings: {
+      		optimizer: {
+      			enabled: true,
+      			runs: 200,
+      		},
+      	}
+      },
+      {
+        version: "0.7.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        }
+      },
+    ],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  // solidity: "0.7.6",
 };
